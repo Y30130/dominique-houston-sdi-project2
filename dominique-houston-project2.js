129f0f6b72998f27b4d60a8d7a9eee404084da1d
Dominique Houston <dominiquehouston@gmail.com>
2013-05-16 20:32:39 -0600

    Initial Release

---
+++ dominique-houston-project2.js
@@ -0,0 +1,31 @@
+// alert("JavaScript works!");
+
+// Dominique Houston
+// SDI 1305
+// Project 2
+// Continued Javascript Learning
+/*
+A recording artist var recordingArist = "Britney Spears" is going on tour and will be performing one secret show per month for one year. showsPerYear = 12. daysOfTheWeek = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"].
+
+Every night she must perform for fans var performShow = function (daysOfTheWeek). At the end of every day she would like to know how many shows are left while var (showsRemain > 0), so please inform her immediately.
+
+For each day that has passed, the number of shows left will decrease (var showNumber = 0; showNumber < daysOfTheWeek.length; showNumber++).
+
+You are not to look at her nor are you allowed to respond directly to her, you must relayMessageTo = function(person,message){...}, to which you will normally receive a reply along the lines of recordingArtist + " says thank you."
+
+Though you may get used to it, you won't be able to help by wonder if (staff.talkToHer()) when she responds.
+*/
+
+
+// My Global variables
+var recordingArtist = "Britney Spears";
+var daysPerWeek = 7;
+var showsPerWeek = 7;
+var songsPerShows = 12;
+var showDurationMins = 60;
+var weeksPerforming = 2;
+var totalShows = 14;
+var daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",];
+var showsBooked = true;
+var soldOut = true;
+var inCity = true;
