415a9d747f8f07f86c688218432d215a599415d4
Dominique Houston <dominiquehouston@gmail.com>
2013-05-17 20:08:53 -0600

    Commit 1
    Forgot to commit quite a few times, made major progress, however. Will
    make very small commits to make up.

--- dominique-houston-project2.js
+++ dominique-houston-project2.js
@@ -5,7 +5,7 @@
 // Project 2
 // Continued Javascript Learning
 /*
-A recording artist var recordingArist = "Britney Spears" is going on tour and will be performing one secret show per month for one year. showsPerYear = 12. daysOfTheWeek = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"].
+A recording artist var recordingArist = "Britney Spears" is going on tour and will be performing one secret show per month for one year. showsPerYear = 12. monthsOfTheYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"].
 
 Every night she must perform for fans var performShow = function (daysOfTheWeek). At the end of every day she would like to know how many shows are left while var (showsRemain > 0), so please inform her immediately.
 
@@ -19,13 +19,113 @@ Though you may get used to it, you won't be able to help by wonder if (staff.tal
 
 // My Global variables
 var recordingArtist = "Britney Spears";
-var daysPerWeek = 7;
-var showsPerWeek = 7;
-var songsPerShows = 12;
-var showDurationMins = 60;
-var weeksPerforming = 2;
-var totalShows = 14;
-var daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",];
+var showsPerMonth = 1;
+var songsPerShow = 6;
+var showDurationMins = 30;
+var weeksPerforming = 12;
+var monthsOfTheYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 var showsBooked = true;
 var soldOut = true;
 var inCity = true;
+var onTour = true;
+var numOfSeasons = 4;
+var seasons = ["Winter","Spring","Summer","Fall"];
+
+
+// Convert Consolge Log into it's on Function, "Say"
+var say = function(message) {console.log(message);};
+
+// My procedure 
+if (recordingArtist == "Britney Spears") {
+	say(recordingArtist + " is going on tour!");
+	say("");
+} 
+else {
+	console.log("We do not have information for that artist.");
+};
+
+
+// My Boolean Function
+// Determine if concert tickets are avaiable
+var checkShowStatus = function(inCity,showsBooked) {
+	verifiedShow = showsBooked && soldOut;
+	if (inCity && showsBooked) {
+		say("Congratulations! " + recordingArtist + " concert tickets are available for purchase.");
+		say("");
+		return verifiedShow;
+	}
+	if (inCity || showsBooked) {
+		say("Regretfully, concert tickets for " + recordingArtist + " are available, but not in your area. Please try another location.");
+		return false;
+	}
+	if (!showsBooked && !inCity){
+		say("We do not have any shows booked for " + recordingArtist + ".");
+		return false;
+	}
+}
+
+
+// My number function + While Loop
+// Loop and count down how many concerts are left
+var showCountdown = function (shows) {
+	countShows = shows - 1;
+	
+	say("The tour begins today. We have a total of " + shows + " shows.");
+	say("");
+	while (shows > 0) {
+		shows = shows - 1;
+		if (shows > 0) {
+			say( recordingArtist +", that was a great show.");
+			say(shows + " left on the tour.");
+			if (shows == 1) {
+			say("");
+					say("This is the last show. Give it all you've got!");
+				}
+		} 
+		else {
+			say("We have reached the end of the tour. Congratuluations on your success, Britney!");
+		}
+		say(" ");
+	
+	}
+	return countShows;
+}
+
+// My array function + For Loop
+
+var songCountUp = function(weeksPerforming,monthsOfTheYear) {
+	songCount = monthsOfTheYear.push("Nevember");
+	for (var i = 0; i < monthsOfTheYear.length; i++){
+		say("You will be performing " + weeksPerforming + " show this " + monthsOfTheYear[i] + ".");
+		
+		if (i > monthsOfTheYear) {
+			say("There are still upcoming performances.");
+		}
+		say(" ");
+	}
+	return songCount;
+}
+
+// String function
+
+var offerDownload = function(freeSong,freeVideo) {
+	downloadMessage = console.log("Download " + freeSong + " or " + freeVideo);
+	return downloadMessage;
+	}
+
+
+// Function Execution
+checkShowStatus(true,true); // toggle boolean values to return different outputs
+showCountdown(12); // edit numerical value to change output
+songCountUp(1,monthsOfTheYear); // For Loop with Array
+say(verifiedShow); // returning that boolean on em
+say(monthsOfTheYear.push("Nevember")); // Pushing and returning from the array function and for loop
+say("My return valued for my number function is " + countShows + "."); // Doing something with returned value of my while loop and number function
+say(songCount); // Doing something with a return value of my array and for loop 
+say(offerDownload("Ooh La La","Scream & Shout")); // String Function 
+
+
+// Returned Values
+say();
+say();
+say();
